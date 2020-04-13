1f101cefc7cf342392d9b55b8ee0d04938e4c03a
Andrew Clifton <andrew.r.clifton3.civ@mail.mil>
2020-04-13 12:04:57 -0400

    Moved calculation to function.

--- main.cpp
+++ main.cpp
@@ -21,10 +21,7 @@ Matrix3 skew(const Vector3& angles) {
 	return {{0, -angles[2], angles[1]}, {angles[2], 0, -angles[0]}, {-angles[1], angles[0], 0}};
 }
 
-
-int main(int argc, char* argv[])
-{
-    Vector3 phi = {.5, 2, 7};
+Matrix3 calculation(Vector3 phi){
     Matrix3 eye3 = {{1, 0, 0},{0, 1, 0},{0, 0, 1}};
 	auto phi_mag   = xt::norm_l2(phi * 2);
 	auto phi_mag2  = phi_mag * phi_mag;
@@ -32,9 +29,14 @@ int main(int argc, char* argv[])
 	auto term1     = 1 - phi_mag2 / 6 + phi_mag4 / 120;
 	auto term2     = 0.5 - phi_mag2 / 24 + phi_mag4 / 720;
 	auto phi_cross = skew(phi);
-	// auto result =  eye3 + term1 * phi_cross + term2 * xt::lin stk::mmul(phi_cross, phi_cross);
 	auto result =  eye3 + term1 * phi_cross + term2 * xt::linalg::dot(phi_cross,phi_cross);
+    return result;
+}
 
+int main(int argc, char* argv[])
+{
+    Vector3 phi = {.5, 2, 7};
+    auto result = calculation(phi);
     double value = result[0];
     std::cout << value <<std::endl;
     struct test a;
